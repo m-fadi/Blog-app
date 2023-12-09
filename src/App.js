9d492e1cc3f8af1826e56909bea3fa4451f17103
fadi <fadi.m.marouf@gmail.com>
2023-12-09 12:52:41 +0100

    add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import { BrowserRouter, Router, Routers } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routers>
+        
+      </Routers>
+      </BrowserRouter>
     </div>
   );
 }
