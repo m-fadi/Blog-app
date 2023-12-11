281ddc6201408328cbf493fbb2e54b42bdb7db84
fadi <fadi.m.marouf@gmail.com>
2023-12-11 17:40:22 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from '../Pages/Home'
+import ArticlesList from '../Pages/ArticlesList'
+import About from '../Pages/About'
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+      <div>
+          <ul>
+              <li>
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+                  <Link to="/articles">Articles</Link>
+              </li>
+              <li>
+                  <Link to="/about"> About</Link>
+              </li>
+          </ul>
+      </div>
+  );
+}
+
+export default Navbar
