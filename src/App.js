3db62c254b688a04416c370a1fa9b32d48580bf1
fadi <fadi.m.marouf@gmail.com>
2023-12-11 17:20:03 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { BrowserRouter, Router, Routers } from "react-router-dom"
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Article from "./Pages/Article";
+import ArticleList from "./Pages/ArticleList";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Routers>
-        
-      </Routers>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/articles/:articleId" element={<Article />} />
+                    <Route path="/articles" element={<ArticleList />} />
+                    <Route path="notFound" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
