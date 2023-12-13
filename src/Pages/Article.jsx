0542e54bd8f4521c68b4e1eaed91ddad6308d936
fadi <fadi.m.marouf@gmail.com>
2023-12-13 23:25:44 +0100

    article paragraph

--- src/Pages/Article.jsx
+++ src/Pages/Article.jsx
@@ -5,12 +5,13 @@ import articleContent from "./articleContent";
 function Article() {
     let {articleId}=useParams()
     const article=articleContent.find(article=> article.name===articleId)
+    const paragraph=article.content.map(paragraph=><p> {paragraph}</p>)
   return (
       <div>
           {" "}
          
           <h2>{article.title}</h2>
-          <p className="articleBody">{article.content}</p>
+          <p >{paragraph}</p>
       </div>
   );
 }
