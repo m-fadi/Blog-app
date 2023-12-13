4dad3204e02d0b8cbb71fec1c8bb37f8fcd6cace
fadi <fadi.m.marouf@gmail.com>
2023-12-13 23:11:45 +0100

    Link

--- src/Pages/ArticlesList.jsx
+++ src/Pages/ArticlesList.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import articleContent from "./articleContent";
 function ArticlesListPage() {
-  return (
-    <div>ArticlesListPage</div>
-  )
+    let articles = articleContent.map((article) => (
+        <div className="article" key={article.name}>
+            <Link to={`/articles/${article.name}`}>
+                <h1>{article.name}</h1>
+                <h2>{article.title}</h2>
+                <p className="articleBody">{article.content}</p>
+                <hr />
+            </Link>
+        </div>
+    ));
+    return <div id="articles">{articles}</div>;
 }
 
-export default ArticlesListPage
+export default ArticlesListPage;
