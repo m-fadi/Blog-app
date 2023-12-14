359bb120016ed351c816346680a71b3a7a831daf
fadi <fadi.m.marouf@gmail.com>
2023-12-14 08:56:23 +0100

    add key to mp

--- src/Pages/Article.jsx
+++ src/Pages/Article.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 import articleContent from "./articleContent";
 
-
 function Article() {
-    let {articleId}=useParams()
-    const article=articleContent.find(article=> article.name===articleId)
-    const paragraph=article.content.map(paragraph=><p> {paragraph}</p>)
-  return (
-      <div >
-          <h2>{article.title}</h2>
-          <p>{paragraph}</p>
-      </div>
-  );
+    let { articleId } = useParams();
+    const article = articleContent.find(
+        (article) => article.name === articleId
+    );
+    const paragraph = article.content.map((paragraph) => (
+        <p key={article.name}> {paragraph}</p>
+    ));
+    return (
+        <div>
+            <h2>{article.title}</h2>
+            <p>{paragraph}</p>
+        </div>
+    );
 }
 
-export default Article
+export default Article;
