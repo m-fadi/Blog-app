05993ae87160e6aeff2435357c3e58614c86360d
fadi <fadi.m.marouf@gmail.com>
2023-12-14 01:03:56 +0100

    x

--- src/Pages/ArticlesList.jsx
+++ src/Pages/ArticlesList.jsx
@@ -5,11 +5,17 @@ import articleContent from "./articleContent";
 function ArticlesListPage() {
     let articles = articleContent.map((article) => (
         <div className="article" key={article.name}>
-            <Link to={`/articles/${article.name}`}>
+            <Link
+                className="article-list-item"
+                to={`/articles/${article.name}`}
+            >
                 <h1>{article.name}</h1>
                 <h2>{article.title}</h2>
-                <p className="articleBody">{article.content}</p>
-                <hr />
+                <p className="articleBody">
+                    {article.content[0].substring(0, 150)}...{" "}
+                    <span style={{ color: "red" }}>see more</span>
+                </p>
+                
             </Link>
         </div>
     ));
