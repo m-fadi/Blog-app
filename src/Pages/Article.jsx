293524e4e8ad65ee7dee380a2bcc853c7af011ba
fadi <fadi.m.marouf@gmail.com>
2023-12-14 12:42:56 +0100

    not found

--- src/Pages/Article.jsx
+++ src/Pages/Article.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import articleContent from "./articleContent";
+import NotFoundPage from "./NotFoundPage";
 
 function Article() {
     let { articleId } = useParams();
     const article = articleContent.find(
         (article) => article.name === articleId
     );
+    if(!article) return <NotFoundPage/>
     const paragraph = article.content.map((paragraph) => (
         <p key={article.name}> {paragraph}</p>
     ));
