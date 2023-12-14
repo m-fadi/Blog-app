2ad82d55f62bf709c4b32c67f57281915ce03e3a
fadi <fadi.m.marouf@gmail.com>
2023-12-14 10:16:28 +0100

    clean

--- src/Pages/ArticlesList.jsx
+++ src/Pages/ArticlesList.jsx
@@ -1,25 +1,15 @@
 import { Link } from "react-router-dom";
 import articleContent from "./articleContent";
+import ArticleBanner from "../components/ArticleBanner";
 
 
 function ArticlesListPage() {
-    let articles = articleContent.map((article) => (
-        <div className="article" key={article.name}>
-            <Link
-                className="article-list-item"
-                to={`/articles/${article.name}`}
-            >
-                <h1>{article.name}</h1>
-                <h2>{article.title}</h2>
-                <p className="articleBody">
-                    {article.content[0].substring(0, 150)}...{" "}
-                    <span style={{ color: "red" }}>see more</span>
-                </p>
-                
-            </Link>
+   
+    return (
+        <div id="articles">
+            <ArticleBanner articles={articleContent}/>
         </div>
-    ));
-    return <div id="articles">{articles}</div>;
+    );
 }
 
 export default ArticlesListPage;
