e000148dd30427d6364fca3169acfbff793ffdae
fadi <fadi.m.marouf@gmail.com>
2023-12-14 10:38:31 +0100

    cln

--- src/components/ArticleBanner.jsx
+++ src/components/ArticleBanner.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ArticleBanner({ articles }) {
-    
     let articleItems = articles.map((article) => (
         <div className="article" key={article.name}>
             <Link
