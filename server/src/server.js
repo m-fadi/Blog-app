56c430d19085ec5e795d5da74a10c1bffe30451d
m-fadi <fadi.m.marouf@gmail.com>
2023-12-21 23:25:48 +0100

    first

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,25 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 import express from "express";
 const app = express();
 app.use(express.json());
-
+const { connectToDb, getDb } = require("../mongo-db-data/db.cjs");
+//import {connectToDb,getDb} from '../mongo-db-data/db'
 app.get("/input", (req, res) => {
     res.send("what r u doing");
 });
+let db;
+connectToDb((err) => {
+    if (!err) {
+        db = getDb();
+       
+    }
+});
+
+app.get('/books',(req,res) => {
+    db.collection('books').find()
+    res.json({msg:"welcome to api"})
+})
 app.post("/input/:name", (req, res) => {
     const { name } = req.params;
     console.log(name);
@@ -44,15 +59,11 @@ app.post("/api/articles/:name/comments", (req, res) => {
     console.log(req.body);
     const { postedBy, text } = req.body;
     const { name } = req.params;
-    const article = articleInfo.find(
-        (article) => article.name === name
-    );
+    const article = articleInfo.find((article) => article.name === name);
     if (article) {
         if (text) {
             article.comments.push(text, postedBy);
             res.send(article.comments);
-            
-            
         } else res.send(``);
         console.log(article.comments);
     } else res.send(" article doesn't exist");
@@ -68,3 +79,27 @@ app.post("/api/articles/:name/comments", (req, res) => {
 //     return updatedInfo
 // }
 app.listen(8000, () => console.log("listening on port 8000"));
+
+[
+    {
+        title: "the what but",
+        author: "the",
+        rating: 5,
+        pages: 400,
+        genres: ["fantasy", "action"],
+    },
+    {
+        title: "ddd",
+        author: "d",
+        rating: 5,
+        pages: 40,
+        genres: ["shit", "tion"],
+    },
+    {
+        title: " but",
+        author: "but",
+        rating: 5,
+        pages: 50,
+        genres: ["comedy", "play"],
+    },
+];
