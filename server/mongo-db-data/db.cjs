f522e2f0a2a9f6b72430c8e4cb584863ab33c6f7
m-fadi <fadi.m.marouf@gmail.com>
2023-12-22 10:04:47 +0100

    clean

--- server/mongo-db-data/db.cjs
+++ server/mongo-db-data/db.cjs
@@ -1,18 +1,23 @@
-const { MongoClient } = require('mongodb')
+// const { MongoClient } = require('mongodb')
 
-let dbConnection
+// let dbConnection
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient.connect("mongodb://0.0.0.0:27017/bookstore")
-        .then((client) => {
-            dbConnection = client.db();
-            return cb();
-        })
-        .catch((err) => {
-            console.log(err);
-            return cb(err);
-        });
-  },
-  getDb: () => dbConnection
-}
+// module.exports = {
+//   connectToDb: (cb) => {
+//     MongoClient.connect("mongodb://0.0.0.0:27017/bookstore")
+//         .then((client) => {
+//             dbConnection = client.db();
+//             return cb();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             return cb(err);
+//         });
+//   },
+//   getDb: () => dbConnection
+// }
+
+// import { MongoClient, mongoClient } from "mongodb";
+// const client = new MongoClient("mongodb://0.0.0.0:27017");
+// await client.connect();
+// const db=client.db('blog')
