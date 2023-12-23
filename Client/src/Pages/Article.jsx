2c9bd29072435a39abf6caa7c9d47a8ed8c7d671
m-fadi <fadi.m.marouf@gmail.com>
2023-12-23 15:14:46 +0100

    x

--- Client/src/Pages/Article.jsx
+++ Client/src/Pages/Article.jsx
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import articleContent from "./articleContent";
 import NotFoundPage from "./NotFoundPage";
-
+import CommentsList from "../components/CommentsList";
 function Article() {
-    const { articleInfo, setArticleInfo } = useState({
-        comments: [],
-        upvotes: 0,
-    });
-    useEffect(()=>{
-        const response=await axios.get(`http://localhost:8000/api/articles/${articleId}`)
-    },[])
-    let { articleId } = useParams();
-    const article = articleContent.find(
-        (article) => article.name === articleId
-    );
-    if(!article) return <NotFoundPage/>
-    const paragraph = article.content.map((paragraph) => (
-        <p key={article.name}> {paragraph}</p>
-    ));
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            <p>this article has {articleInfo.upvotes} upvotes</p>
-            <p>{paragraph}</p>
-        </div>
-    );
+     const [articleInfo, setArticleInfo] = useState({
+         upvotes: 0,
+         comments: [],
+     });
+     const { articleId } = useParams();
+
+     useEffect(() => {
+         const loadArticleInfo = async () => {
+             const response = await axios.get(`/api/articles/${articleId}`);
+             const newArticleInfo = response.data;
+             setArticleInfo(newArticleInfo);
+             
+         };
+
+         loadArticleInfo();
+         console.log(articleInfo);
+     }, [articleId]);
+
+     const article = articleContent.find((article) => article.name === articleId);
+// const commentsHtml=articleInfo.comments.map(comment=>{
+//     console.log(comment)
+//    return  <div>
+       
+
+//     </div>})
+    console.log( articleInfo.comments );
+     if (!articleInfo) {
+         return <NotFoundPage />;
+     }
+
+     return (
+         <>
+             <h1>{articleInfo.name}</h1>
+             <p>This article has {articleInfo.upvotes} upvote(s)</p>
+
+             {article.content.map((paragraph, i) => (
+                 <p key={i}>{paragraph}</p>
+             ))}
+<h3>Comments: </h3>
+             <CommentsList comments={articleInfo.comments} />
+         </>
+     );
+
+     
 }
 
 export default Article;
