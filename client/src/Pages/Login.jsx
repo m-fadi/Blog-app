60e3bb0b20352cf9b3c7de21ab260cd199f05e02
m-fadi <fadi.m.marouf@gmail.com>
2023-12-28 10:11:28 +0100

    login com

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import {useState} from 'react'
+import Register from './Register'
+import { Link } from 'react-router-dom'
 function Login() {
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [error,setError]=useState("")
   return (
-    <div>
-      
-    </div>
-  )
+      <div className='login'>
+          <h1> Login</h1>
+          <input
+              type="text"
+              name="email"
+              placeholder="E-Mail"
+              onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button> Login</button>
+          <Link to="/register" element={<Register />}>
+              <h6>You don't have an account ? Register</h6>{" "}
+          </Link>
+      </div>
+  );
 }
 
 export default Login
