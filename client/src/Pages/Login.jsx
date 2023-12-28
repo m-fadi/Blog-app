9a6c4b36c04aefa7010c2508df8241dbab1c8413
m-fadi <fadi.m.marouf@gmail.com>
2023-12-28 10:56:07 +0100

    login logic

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,32 +1,47 @@
-import {useState} from 'react'
-import Register from './Register'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import Register from "./Register";
+import { Link } from "react-router-dom";
+import axios from "axios";
 function Login() {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  const [error,setError]=useState("")
-  return (
-      <div className='login'>
-          <h1> Login</h1>
-          <input
-              type="text"
-              name="email"
-              placeholder="E-Mail"
-              onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-              type="text"
-              name="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-          />
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [user, setUser] = useState({
+        firstName: "",
+        lastName:"",
+        email:""
+    });
+    const data = {
+        password,
+        email,
+    };
+    const handleLogin = async () => {
+        const response = await axios.post("/api/login", data);
+        setUser(response.user);
+        console.log(response);
+    };
+    return (
+        <div className="login">
+            <h1> Login</h1>
+            <input
+                type="text"
+                name="email"
+                placeholder="E-Mail"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="text"
+                name="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
-          <button> Login</button>
-          <Link to="/register" element={<Register />}>
-              <h6>You don't have an account ? Register</h6>{" "}
-          </Link>
-      </div>
-  );
+            <button onClick={handleLogin}> Login </button>
+            <Link to="/register" element={<Register />}>
+                <h6>You don't have an account ? Register</h6>{" "}
+            </Link>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
