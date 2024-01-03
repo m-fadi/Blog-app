f288ec7d285a8db1d4be3238217fb74a4f2c2901
m-fadi <fadi.m.marouf@gmail.com>
2024-01-04 00:14:20 +0100

    clean

--- client/src/Pages/Article.jsx
+++ client/src/Pages/Article.jsx
@@ -3,13 +3,16 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
-
+import useUser from "../hooks/useUser";
+import { Link } from "react-router-dom";
+import Login from "./Login";
 function Article() {
     const [articleInfo, setArticleInfo] = useState({
         upvotes: 0,
         comments: [],
     });
     const { articleId } = useParams();
+    const {user, isloading}=useUser()
     const handleUpvote = async () => {
         const response = await axios.put(`/api/articles/${articleId}/upvote`);
         const newArticleInfo = response.data;
@@ -40,10 +43,17 @@ function Article() {
 
             <div className="upvotes-section">
                 <p>
-                    This article has {articleInfo.upvotes} {articleInfo.upvotes <1 ? "upvotes" : "upvotes"}
-                </p>
-
-                <button onClick={handleUpvote}>UpVote</button>
+                    This article has {articleInfo.upvotes}{" "}
+                    {articleInfo.upvotes < 1 ? "upvotes" : "upvotes"}
+                    {user ? (
+                        <button onClick={handleUpvote}>UpVote</button>
+                    ) : (
+                        <Link to="/login" element={<Login />}>
+                            {" "}
+                            Login to vote
+                        </Link>
+                    )}
+                </p>{" "}
             </div>
             <CommentsList
                 comments={articleInfo.comments}
