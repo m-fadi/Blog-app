bbb4c8929ea6a1979366e19f79f665c2a249ba36
m-fadi <fadi.m.marouf@gmail.com>
2024-01-04 09:26:59 +0100

    b

--- client/src/components/CommentsList.jsx
+++ client/src/components/CommentsList.jsx
@@ -1,25 +1,7 @@
-import { useState } from "react";
-import axios from "axios";
-
-function CommentsList({ comments, articleId, setArticleInfo }) {
-
-    const [name, setName] = useState("");
-    const [comment, setComment] = useState("");
 
 
-    // --- submit comment function
-    const handleComment = async () => {
-        const response = await axios.post(
-            `/api/articles/${articleId}/comments`,
-            {
-                postedBy: name,
-                text: comment,
-            }
-        );
-        setArticleInfo(response.data);
-        setComment("");
-        setName("");
-    };
+
+function CommentsList({ comments }) {
 
 
     /// --- map comments and build the html---
@@ -32,24 +14,9 @@ function CommentsList({ comments, articleId, setArticleInfo }) {
 
     return (
         <div>
-            <div id="add-comment-form">
-                <input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    type="text"
-                    placeholder="add your name"
-                />
-                <textarea
-                    value={comment}
-                    name="comment"
-                    onChange={(e) => setComment(e.target.value)}
-                    cols="50"
-                    rows="5"
-                    placeholder="add your comment"
-                ></textarea>
-                <button onClick={handleComment}> Submit </button>
-            </div>
-            <h3>Comments: </h3>{commentsHtml}
+            
+            <h3>Comments: </h3>
+            {commentsHtml}
         </div>
     );
 }
